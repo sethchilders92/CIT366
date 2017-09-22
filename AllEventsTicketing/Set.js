157428a0a77f97e525bdfc7b03b289c0db48585e
Seth Childers <chi11008@byui.edu>
2017-09-21 21:11:45 -0600

    Final with better indentation

--- AllEventsTicketing/Set.js
+++ AllEventsTicketing/Set.js
@@ -1,87 +1,87 @@
 function Set() {
 
 
-	this.intersection = function(listA, listB) {
+    this.intersection = function(listA, listB) {
 
         if (listA === null || listB === null) {
             return null;
         }
 
-	    var resultList = [];
+        var resultList = [];
 
-	    for (var i =0; i < listA.length; i++) {
-	   		var nextValue = listA[i];
+        for (var i =0; i < listA.length; i++) {
+            var nextValue = listA[i];
 
-	   		for (var j = 0; j < listB.length; j++) {
-	   			if (listB[j] === nextValue) {
-	   				resultList.push(listB[j]);
-	   				break;
-				}
-			}
-	    }
+            for (var j = 0; j < listB.length; j++) {
+                if (listB[j] === nextValue) {
+                    resultList.push(listB[j]);
+                    break;
+                }
+            }
+        }
 
-	   return resultList;
-	}
+        return resultList;
+    }
 
 
-	this.union = function(listA, listB) {
+    this.union = function(listA, listB) {
 
         if (listA === null || listB === null) {
             return null;
         }
 
-	    var resultList = [];
+        var resultList = [];
 
-	    var tempA = this.relativeComplement(listA, listB);
-	    resultList = tempA.concat(listB);
+        var tempA = this.relativeComplement(listA, listB);
+        resultList = tempA.concat(listB);
 
-	    return resultList;
-	}
-
-
+        return resultList;
+    }
 
 
-	this.relativeComplement = function(listA, listB) {
 
-		if (listA === null || listB === null) {
+
+    this.relativeComplement = function(listA, listB) {
+
+        if (listA === null || listB === null) {
             return null;
         }
 
         var resultList = listA.slice(0);
 
-	    var intersect = this.intersection(listA, listB);
+        var intersect = this.intersection(listA, listB);
 
-	    for (var i=0; i < intersect.length; i++) {
-		   for (var j=0; j < listA.length; j++) {
-		   		if (intersect[i] === listA[j]) {
-                	var index = resultList.indexOf(listA[j]);
-                	if (index > -1) {
-                    	resultList.splice(index, 1);
-					}
-					break;
-            	}
-		   }
-	   }
+        for (var i=0; i < intersect.length; i++) {
+            for (var j=0; j < listA.length; j++) {
+                if (intersect[i] === listA[j]) {
+                    var index = resultList.indexOf(listA[j]);
+                    if (index > -1) {
+                        resultList.splice(index, 1);
+                    }
+                    break;
+                }
+            }
+        }
         return resultList;
-	}
+    }
 
 
 
-	this.symmetricDifference = function(listA, listB) {
+    this.symmetricDifference = function(listA, listB) {
 
-		if (listA === null || listB === null) {
+        if (listA === null || listB === null) {
             return null;
         }
 
-	    var resultList = [];
+        var resultList = [];
 
-	    var tempA = this.relativeComplement(listA, listB);
-	    var tempB = this.relativeComplement(listB, listA);
+        var tempA = this.relativeComplement(listA, listB);
+        var tempB = this.relativeComplement(listB, listA);
 
-	    resultList = tempA.concat(tempB);
+        resultList = tempA.concat(tempB);
 
-	    return resultList;
-	}
+        return resultList;
+    }
 
 
 }
