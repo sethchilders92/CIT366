6c6141e4ea29a1f0b3a8bff63b9b6ecf85a18a3d
Seth Childers <chi11008@byui.edu>
2017-09-28 13:08:19 -0600

    Committing all my work

---
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contacts.model';
+import { ContactListComponent} from '../contact-list/contact-list.component';
+
+@Component({
+  selector: 'cms-contact-detail',
+  templateUrl: './contact-detail.component.html',
+  styleUrls: ['./contact-detail.component.css']
+})
+export class ContactDetailComponent implements OnInit {
+  contacts: Contact;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
